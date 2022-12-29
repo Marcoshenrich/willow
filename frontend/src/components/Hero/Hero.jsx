7bcab8cfae809431bfde67fcb6887b87633379a2
Marcos <jmarcoshenrich@gmail.com>
2022-12-28 18:16:43 -0800

    searchbar layout complete

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import "./Hero.css"
 import blueMyco from "../assets/heroes/bluemyco.jpg"
+import { BsSearch } from "react-icons/bs"
 
 const Hero = () => {
     return (
@@ -11,6 +12,7 @@ const Hero = () => {
             <div>Discover Your Fairy Tale</div>
             <div id="Search-Bar">
                 <input type="search" placeholder="find your dream home"/>
+                    <button id="Search-Button">{<BsSearch id="Search-Glass"/>}</button>
             </div>
             
         </div>
