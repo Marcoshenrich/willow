ced8514bc1d4740ac69daa2267eab860c9fef941
Marcos <jmarcoshenrich@gmail.com>
2023-01-01 23:36:55 -0800

    LSP Core Info restructured

--- frontend/src/components/ListingShowPage/ListingShowPage.jsx
+++ frontend/src/components/ListingShowPage/ListingShowPage.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { fetchListing, getListing } from "../../store/listings"
 import "./ListingShowPage.css"
-import { TbBed } from "react-icons/tb"
+import ListingShowPageCoreInfo from "./ListingShowPageCoreInfo/ListingShowPageCoreInfo"
 
 const ListingShowPage = () => {
   const dispatch = useDispatch()
@@ -14,40 +14,18 @@ const ListingShowPage = () => {
     dispatch(fetchListing(listingId))
   }, [listingId])
 
+
   return (
-    <div className="Listing-Show-Page">
-      <div className="Listing-Show-Page-Images">images</div>
-      <div className="Listing-Show-Page-Core-Info">
-        <div>
-          <div className="Listing-Show-Page-Address">
-            {listing && (<p id="Listing-Show-Page-Core-Info-h2">{listing.streetNum} {listing.streetName}</p>)}
-            {listing && (<p id="Listing-Show-Page-Core-Info-Body">{listing.city}, {listing.state}, {listing.zip}</p>)}
-          </div>
-            <div id="Listing-Show-Page-Base-Stats">
-            <TbBed className="Listing-Show-Page-Icon"/>
-            <div>3 beds</div>
-            <TbBed className="Listing-Show-Page-Icon" />
-            <div>2 Baths</div>
-            <TbBed className="Listing-Show-Page-Icon" />
-            <div>1,300 sqft</div>
-          </div>
-        </div>
-        <div className="Listing-Show-Page-Mortgage-Info">
-          {listing && (<p id="Listing-Show-Page-Core-Info-h2">{"$" + listing.value} </p>)}
-          {listing && (<p id="Listing-Show-Page-Core-Info-Body">{"Est. Mortgage $" + Math.floor(listing.value/24) + "/mo*"} </p>)}
-          {listing && (<p id="Listing-Show-Page-Core-Info-Body">{"40 human teeth deposit"} </p>)}
-        </div>
-        <div id="Listing-Show-Page-Appointment-Info">
-          Request a tour 
-          <button>Schedule a tour</button>
-        </div>
-      </div>
-      <div id="Listing-Show-Page-Local-Info">Local Info</div>
-      <div id="Listing-Show-Page-Description">Description
+    <div className="LSP">
+      <div className="LSP-Images">images</div>
+      <ListingShowPageCoreInfo listing={listing}/>
+      <div id="LSP-Local-Info">Local Info</div>
+      <div id="LSP-Description">
+        <h2>Description</h2>
         {listing && (<p>{listing.description}</p>)}
       </div>
-      <div id="Listing-Show-Page-Highlights">Highlights</div>
-      <div id="Listing-Show-Page-Details">Details</div>
+      <div id="LSP-Highlights">Highlights</div>
+      <div id="LSP-Details">Details</div>
     </div>
   )
 }
---
+++ frontend/src/components/ListingShowPage/ListingShowPageCoreInfo/ListingShowPageCoreInfo.jsx
@@ -0,0 +1,40 @@
+import "./ListingShowPageCoreInfo.css"
+import { TbBed } from "react-icons/tb"
+
+const ListingShowPageCoreInfo = ({listing}) => {
+
+  return (
+    <>
+      <div className="LSP-Core-Info">
+        <div className="LSP-Top-Details">
+          <div>
+            <div className="LSP-Address">
+              {listing && (<p id="LSP-Core-Info-h2">{listing.streetNum} {listing.streetName}</p>)}
+              {listing && (<p id="LSP-Core-Info-Body">{listing.city}, {listing.state}, {listing.zip}</p>)}
+            </div>
+            <div id="LSP-Base-Stats">
+              <TbBed className="LSP-Icon" />
+              <div>3 beds</div>
+              <TbBed className="LSP-Icon" />
+              <div>2 Baths</div>
+              <TbBed className="LSP-Icon" />
+              <div>1,300 sqft</div>
+            </div>
+          </div>
+          <div className="LSP-Mortgage-Info">
+            {listing && (<p id="LSP-Core-Info-h2">{"$" + listing.value} </p>)}
+            {listing && (<p id="LSP-Core-Info-Body">{"Est. Mortgage $" + Math.floor(listing.value / 24) + "/mo*"} </p>)}
+            {listing && (<p id="LSP-Core-Info-Body">{"40 human teeth deposit"} </p>)}
+          </div>
+        </div>
+        <div className="LSP-Appointment-Info">
+          <p>Request a tour as early as</p>
+          <p id="LSP-Appointment-Time">Tomorrow at 2:30pm</p>
+          <button id="LSP-Appointment-Submit">Schedule a Tour</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ListingShowPageCoreInfo
