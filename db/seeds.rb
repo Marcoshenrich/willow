e503f569ab06c235ef08a0bd8de3fef128ea722d
Marcos <jmarcoshenrich@gmail.com>
2023-01-04 14:58:19 -0800

    render seeds reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ ApplicationRecord.transaction do
 
   puts "Destroying tables..."
   # Unnecessary if using `rails db:seed:replant`
-  User.destroy_all
   Listing.destroy_all
+  User.destroy_all
 
   puts "Resetting primary keys..."
   # For easy testing, so that after seeding, the first `User` has `id` of 1
