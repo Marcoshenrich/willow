f675edf6d1f45d1449c5ae234a0dc10067579a3d
Marcos <jmarcoshenrich@gmail.com>
2023-01-10 15:25:16 -0800

    LSP review module skeleton set

--- frontend/src/components/ListingShowPage/LSPReviews/LSPReviewModule/LSPReviewModule.jsx
+++ frontend/src/components/ListingShowPage/LSPReviews/LSPReviewModule/LSPReviewModule.jsx
@@ -1,10 +1,19 @@
 import "./LSPReviewModule.css"
+import { TiDeleteOutline } from "react-icons/ti"
+import { BsPencil } from "react-icons/bs"
 
 const LSPReviewModule = () => {
 
   return (
     <div className="LSP-Review-Module">
-      <div id="LSPRM-Username" >HelenOfTroy</div>
+      <div id="LSPRM-Top-Bar" >
+        <div id="LSPRM-Username">HelenOfTroy</div>
+        <div id="LSPRM-Top-Bar-Icons">
+          <BsPencil />
+          <TiDeleteOutline />
+        </div>
+      
+      </div>
       <div id="LSPRM-Date">Visited on January 12</div>
       <div id="LSPRM-Body">This place was super cute, the chantrelles were just starting to spore</div>
     </div>
